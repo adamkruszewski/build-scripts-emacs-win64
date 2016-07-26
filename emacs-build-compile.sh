74638b9d033f77394196af94632e9dbd647de2ba
Adam Kruszewski <adam@kruszewski.name>
2016-07-26 18:07:39 +0200

    Fixed getting pulled repository to pristine state.

--- emacs-build-compile.sh
+++ emacs-build-compile.sh
@@ -8,7 +8,7 @@ PREFIX=/c/emacs/emacs-25
 [ -d emacs-25 ] || git clone git://git.sv.gnu.org/emacs.git emacs-25
 cd emacs-25
 git config core.autocrlf false
-git reset --hard
+git reset --hard && git clean -dfx
 git pull
 ./autogen.sh
 CFLAGS=-O2 PKG_CONFIG_PATH=/mingw64/lib/pkgconfig \
